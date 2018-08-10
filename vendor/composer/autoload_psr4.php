c4c6a928aab8289e82d224d4c52afb2509920c67
Goufoux <deadworldcorp@gmail.com>
2018-08-10 14:27:45 +0200

    Mise à jour des infos d'autoload (ajout lib core)

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,5 +6,6 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Core\\' => array($baseDir . '/lib/Core'),
     'App\\' => array($baseDir . '/App'),
 );
