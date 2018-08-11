d9c226bb88f2ea485223e81697c2dfad369b5c20
Goufoux <deadworldcorp@gmail.com>
2018-08-11 14:09:44 +0200

    Revert "Mise à jour de l'index, ajout d'un menu de navigation, d'une section composé de deux colonnes, une qui contiendra les outils de la page admin, et l'autre, le contenu de la vue en cours"
    This reverts commit 2c2e835ffeff5eeec43301bddb15ef4d12b5b621.

--- App/Backend/Modules/Index/Views/index.php
+++ App/Backend/Modules/Index/Views/index.php
@@ -1,6 +1,1 @@
-<link rel="stylesheet" href="../css/backend/index.css" />
-<h2> Vue index backend </h2>
-<p>
-	Chemin de la page: <?php echo __DIR__; ?> <br />
-	Requête URL: <?php echo $_SERVER['REQUEST_URI']; ?> 
-</p>
+<h1> Page d'accueil Admin - Backend</h1>
