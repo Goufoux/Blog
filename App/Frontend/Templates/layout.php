05fcc3c322f72d24218b5afe5255468fa9311736
Goufoux <deadworldcorp@gmail.com>
2018-08-11 12:02:17 +0200

    Ajout des libraires bootstrap et jquery au template ainsi que de l'icone de page

--- App/Frontend/Templates/layout.php
+++ App/Frontend/Templates/layout.php
@@ -2,6 +2,10 @@
 <html>
 	<head>
 		<meta charset="utf-8" />
+		<link rel="stylesheet" href="../Web/css/bootstrap.min.css" />
+		<script src="../Web/js/bootstrap.min.js"></script>
+		<script src="../Web/js/jquery-3.3.1.min.js"></script>
+		<link rel="icon" type="image/x-icon" href="../Web/img/favicon.ico" />
 		<title><?= isset($title) ? $title : 'Genarkys' ?></title>
 	</head>
   
