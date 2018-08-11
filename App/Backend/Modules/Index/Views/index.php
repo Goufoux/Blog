2c2e835ffeff5eeec43301bddb15ef4d12b5b621
Goufoux <deadworldcorp@gmail.com>
2018-08-11 14:08:45 +0200

    Mise à jour de l'index, ajout d'un menu de navigation, d'une section composé de deux colonnes, une qui contiendra les outils de la page admin, et l'autre, le contenu de la vue en cours

--- App/Backend/Modules/Index/Views/index.php
+++ App/Backend/Modules/Index/Views/index.php
@@ -1,1 +1,6 @@
-<h1> Page d'accueil Admin - Backend</h1>
+<link rel="stylesheet" href="../css/backend/index.css" />
+<h2> Vue index backend </h2>
+<p>
+	Chemin de la page: <?php echo __DIR__; ?> <br />
+	Requête URL: <?php echo $_SERVER['REQUEST_URI']; ?> 
+</p>
