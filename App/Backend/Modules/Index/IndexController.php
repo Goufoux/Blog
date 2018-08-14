c83aab85707e5eb284ef0f1e7bfc5a5a5f378c27
Goufoux <deadworldcorp@gmail.com>
2018-08-14 09:28:33 +0200

    Mise à jour du contrôlleur Backend, ajout de la vue ajouter

--- App/Backend/Modules/Index/IndexController.php
+++ App/Backend/Modules/Index/IndexController.php
@@ -4,6 +4,8 @@
 	
 	use \Core\HTTPRequest;
 	use \Core\BackController;
+	use \Entity\Billet;
+	
 	/*
 	 * author: Genarkys
 	 *
@@ -21,6 +23,38 @@
 		public function executeAdd(HTTPRequest $HTTPRequest)
 		{
 			$this->page->addVar('title', 'Genarkys - Admin - Add');
-			$this->page->addVar('req', $HTTPRequest->getData('bTitle'));
+			
+			/* On vérifie la page pour voir si il y a des données POST qui ont été transmise (données d'ajout du billet) */
+			if($HTTPRequest->postExists('bTitle') AND !empty($HTTPRequest->postData('bTitle')))
+			{
+				/* On accède au manager des Billets */
+				$bManager = $this->managers->getManagerOf('Billet');
+				$billet = new Billet([
+				'titre' => $HTTPRequest->postData('bTitle'),
+				'contenu' => $HTTPRequest->postData('bDesc')
+				]);
+				/* On vérifie si il y a des erreurs */
+				if(!$billet->getErreurs())
+				{
+					$e = $bManager->addBillet($billet);
+					if($e)
+					{
+						$this->page->addVar('success', $bManager->getManagerError());
+					}
+					else
+					{
+						$this->page->addVar('error', $bManager->getManagerError());
+					}
+				}
+				else
+				{
+					$this->page->addVar('error', $billet->getErreurs());
+				}
+			}
+			else
+			{
+				$this->page->addVar('req', 'Formulaire non détécté');
+				$this->page->addVar('method', $HTTPRequest->method());
+			}
 		}
 	}
