961ca704c1a3f1c98f648887a235c846691c6948
Goufoux <deadworldcorp@gmail.com>
2018-08-14 09:30:13 +0200

    Mise à jour du layout

--- App/Backend/Templates/layout.php
+++ App/Backend/Templates/layout.php
@@ -6,6 +6,7 @@
 		<link rel="stylesheet" href="../css/backend/index.css" />
 		<script src="../js/bootstrap.min.js"></script>
 		<script src="../js/jquery-3.3.1.min.js"></script>
+		<script src="../js/index.js"></script>
 		<link rel="icon" type="image/x-icon" href="../img/logo.png" />
 		<title><?= isset($title) ? $title : 'Genarkys' ?></title>
 	</head>
@@ -13,7 +14,7 @@
 	<body>
 		<!-- NAVBAR -->
 		<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
-			<a class="navbar-brand" href="."> Jean Forteroche </a>
+			<a class="navbar-brand" href="http://genarkys/openclassroom/Blog/Web/"> Jean Forteroche </a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
