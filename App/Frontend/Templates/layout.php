f1e0280cfa214538d03061935414e2a73ef454d8
Goufoux <deadworldcorp@gmail.com>
2018-08-14 17:25:21 +0200

    Maj du layout, réorganisation du menu

--- App/Frontend/Templates/layout.php
+++ App/Frontend/Templates/layout.php
@@ -4,16 +4,15 @@
 		<meta charset="utf-8" />
 		<link rel="stylesheet" href="css/bootstrap.min.css" />
 		<link rel="stylesheet" href="css/frontend/index.css" />
-		<script src="js/bootstrap.min.js"></script>
 		<script src="js/jquery-3.3.1.min.js"></script>
-		<script src="js/index.js"></script>
+		<script src="js/bootstrap.min.js"></script>
 		<link rel="icon" type="image/x-icon" href="img/logo.png" />
 		<title><?= isset($title) ? $title : 'Genarkys' ?></title>
 	</head>
   
 	<body>
 		<!-- NAVBAR -->
-		<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+		<nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
 			<a class="navbar-brand" href="http://genarkys/openclassroom/Blog/Web/"> Jean Forteroche </a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
@@ -23,17 +22,38 @@
 					<li class="nav-item">
 						<a href="#" class="nav-link noActive" title="Roman"> Billet simple pour l'Alaska </a>
 					</li>
-					<li class="nav-item">
-						<a href="http://genarkys/openclassroom/Blog/Web/admin/" class="nav-link noActive" title="Admin"> Admin </a>
-					</li>
+					<?php
+						if(!empty($_SESSION['auth']) AND $_SESSION['auth'])
+						{
+							if($_SESSION['membre']->getAccessLevel() > 1)
+							{
+								?>
+									<li class="nav-item">
+										<a href="admin/" class="nav-link" title="deco"> Admin </a>
+									</li>
+								<?php
+							}
+							?>
+								<li class="nav-item">
+									<a href="deconnect" class="nav-link" title="deco"> Déco </a>
+								</li>
+							<?php
+						}
+						else
+						{
+							?>
+								<li class="nav-item">
+									<a href="connect" class="nav-link" title="connexion"> Connexion </a>
+								</li>
+							<?php
+						}
+					?>
 				</ul>
 			</div>
 		</nav>
 		<div class="bloc"></div>
-		<div class="container-fluid">
-			<div class="row">
+		<section class="container-fluid">
 				<?= $content; ?>
-			</div>
-		</div>
+		</section>
 	</body>
 </html>
