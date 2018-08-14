00d909dd4dfc7a1292ba42c72c6976ffb890f2dc
Goufoux <deadworldcorp@gmail.com>
2018-08-14 09:33:19 +0200

    Mise à jour du contrôlleur frontend, ajout de la méthode qui affichera le billet détaillée, la gestion des commentaires

--- App/Frontend/Modules/Index/IndexController.php
+++ App/Frontend/Modules/Index/IndexController.php
@@ -4,7 +4,7 @@
 	
 	use \Core\HTTPRequest;
 	use \Core\BackController;
-	use \Core\LinkCreator;
+	use \Entity\Comment;
 	
 	/*
 	 * author: Genarkys
@@ -16,6 +16,30 @@
 	{
 		public function executeIndex(HTTPRequest $HTTPRequest)
 		{
-			$this->page->addVar('title', 'Genarkys - Blog');
+			$nbBillet = $this->app->config()->get('nbBillet');
+			$this->page->addVar('title', 'Genarkys - ' . $nbBillet . ' derniers billets');
+			$billet = $this->managers->getManagerOf('Billet')->getBillet();
+			$this->page->addVar('billet', $billet);
+		}
+		
+		public function executeShowBillet(HTTPRequest $HTTPRequest)
+		{
+			$this->page->addVar('title', 'Genarkys - Billet');
+			$billet = $this->managers->getManagerOf('Billet')->getBillet($HTTPRequest->getData('id'));
+			$this->page->addVar('billet', $billet);
+			
+			/* On regarde si des données pour un commentaire sont présente */
+			if($HTTPRequest->postExists('cName'))
+			{
+				// $this->page->addVar('comment', true);
+				$comment = new Comment([
+					'name' => $HTTPRequest->postData('cName'),
+					'contenu' => $HTTPRequest->postData('cDesc'),
+				]);
+				/*  */
+				$e = count($comment->getErreurs());
+				$this->page->addVar('success', $e);
+			}
+			
 		}
 	}
