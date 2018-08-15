9c1d9ad9d4a93ef44a314934f2ca477f1736862c
Goufoux <deadworldcorp@gmail.com>
2018-08-15 15:17:17 +0200

    Ajout d'une nouvelle vue dans le frontend, qui affiche l'ensemble des billets

---
+++ App/Frontend/Modules/Index/Views/listBillet.php
@@ -0,0 +1,28 @@
+<link rel="stylesheet" href="css/frontend/list.css" />
+<h3> Billet Simple pour l'Alaska </h3>
+<blockquote class="col-12">
+	Liste de tous les billets !
+</blockquote>
+<?php
+	if(!empty($list))
+	{
+		foreach($list as $billet)
+		{
+			$date = getDate($billet->getDatePub());
+			?>
+				<a href="billet-<?php echo $billet->getId(); ?>"> <article class="billet col-4">
+					<h3> <?php echo $billet->getTitre(); ?> </h3>
+					<ul>
+						<li> Publié le: <?php echo $date['mday'] . '/' . $date['mon'] . '/' . $date['year']; ?> </li>
+					</ul>
+				</article> </a>
+			<?php
+		}
+	}
+	else
+	{
+		?>
+			<h4> Aucun billet pour le moment... </h4>
+		<?php
+	}
+?>
