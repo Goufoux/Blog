c96d51329848646ee237958ae92e8cfa47ad1291
Goufoux <deadworldcorp@gmail.com>
2018-08-15 15:09:15 +0200

    Mise à jour des input avec l'ajout du champ required

--- App/Frontend/Modules/Index/Views/connect.php
+++ App/Frontend/Modules/Index/Views/connect.php
@@ -1,8 +1,8 @@
 <link rel="stylesheet" href="css/frontend/connect.css" />
 <h3> Connexion </h3>
 <form action="#" method="post" class="formConnect col-4">
-	<input type="text" id="uLogin" class="uLogin" placeholder="Pseudo ou Email" name="uLogin" />
-	<input type="password" id="uPass" class="uPass" placeholder="Pass" name="uPass" />
+	<input type="text" id="uLogin" class="uLogin" placeholder="Pseudo ou Email" name="uLogin" required />
+	<input type="password" id="uPass" class="uPass" placeholder="Pass" name="uPass" required />
 	<input type="submit" value="Connexion" />
 </form>
 <?php
@@ -10,9 +10,8 @@
 	{
 		?>
 			<article class="error">
-				<p>
-					<?php echo $error; ?>
-				</p>
+				<img src="img/croix.png" class="croix" alt="Fermer ?" title="Fermer ?" />
+				<h4> <?php echo $error; ?> </h4>
 			</article>
 		<?php
 	}
