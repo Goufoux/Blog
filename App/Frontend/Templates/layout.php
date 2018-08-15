7dae9ae85ba0a631f936a053ca302dfbd9460cca
Goufoux <deadworldcorp@gmail.com>
2018-08-15 17:05:29 +0200

    Mise à jour des propriétés bootstrap

--- App/Frontend/Templates/layout.php
+++ App/Frontend/Templates/layout.php
@@ -2,8 +2,10 @@
 <html>
 	<head>
 		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width" />
 		<link rel="stylesheet" href="css/bootstrap.min.css" />
 		<link rel="stylesheet" href="css/frontend/index.css" />
+		<link rel="stylesheet" media="screen and (min-width: 768px)" href="css/frontend/md.css" />
 		<script src="js/jquery-3.3.1.min.js"></script>
 		<script src="js/bootstrap.min.js"></script>
 		<script src="js/index.js"></script>
