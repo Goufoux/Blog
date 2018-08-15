7d8f9d2c76c3c19862d6268c89dc1bb8248e8be6
Goufoux <deadworldcorp@gmail.com>
2018-08-15 15:10:48 +0200

    Mise à jour de l'affichage des billets

--- App/Frontend/Templates/layout.php
+++ App/Frontend/Templates/layout.php
@@ -6,6 +6,7 @@
 		<link rel="stylesheet" href="css/frontend/index.css" />
 		<script src="js/jquery-3.3.1.min.js"></script>
 		<script src="js/bootstrap.min.js"></script>
+		<script src="js/index.js"></script>
 		<link rel="icon" type="image/x-icon" href="img/logo.png" />
 		<title><?= isset($title) ? $title : 'Genarkys' ?></title>
 	</head>
@@ -13,14 +14,14 @@
 	<body>
 		<!-- NAVBAR -->
 		<nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
-			<a class="navbar-brand" href="http://genarkys/openclassroom/Blog/Web/"> Jean Forteroche </a>
+			<a class="navbar-brand" href="https://genarkys.fr/openclassroom/Blog/Web/"> Jean Forteroche </a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
 			<div class="collapse navbar-collapse" id="navbarNavDropdown">
 				<ul class="navbar-nav">
 					<li class="nav-item">
-						<a href="#" class="nav-link noActive" title="Roman"> Billet simple pour l'Alaska </a>
+						<a href="list" class="nav-link noActive" title="Roman"> Billet simple pour l'Alaska </a>
 					</li>
 					<?php
 						if(!empty($_SESSION['auth']) AND $_SESSION['auth'])
