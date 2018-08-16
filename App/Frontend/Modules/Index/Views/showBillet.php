4e9efb2eaf9908146a08bd0538602d6b5720e01e
Goufoux <deadworldcorp@gmail.com>
2018-08-16 15:06:42 +0200

    Mise à jour du formulaire

--- App/Frontend/Modules/Index/Views/showBillet.php
+++ App/Frontend/Modules/Index/Views/showBillet.php
@@ -34,14 +34,19 @@
 						?>
 			</article>
 			<section class="boxComment col-12">
-				<form action="#" method="post" class="col-lg-3 col-md-8 col-sm-12 formComment">
-					<h4> Laissez un commentaire ! </h4>
-					<input type="name" id="cName" class="cName col-12" name="cName" placeholder="Nom" required />
-					<input type="email" id="cEmail" class="cEmail col-12" name="cEmail" placeholder="Email" required />
-					<input type="hidden" name="bId" value="<?php echo $billet->getId();?>" />
-					<textarea class="cDesc col-12" id="cDesc" name="cDesc" placeholder="Commentaire..." required></textarea>
-					<input type="submit" value="Commenter" />
-				</form>
+				<?php
+					if(!empty($_SESSION['auth']))
+					{
+						?>
+							<form action="#" method="post" class="col-lg-3 col-md-8 col-sm-12 formComment">
+								<h4> Laissez un commentaire ! </h4>
+								<input type="hidden" name="bId" value="<?php echo $billet->getId();?>" />
+								<textarea class="cDesc col-12" id="cDesc" name="cDesc" placeholder="Commentaire..." required></textarea>
+								<input type="submit" value="Commenter" />
+							</form>
+						<?php
+					}
+				?>
 				<div class="comment col-lg-7 col-md-8 col-sm-12">
 					<h3> Liste des commentaires : </h3>
 						<?php
@@ -59,9 +64,22 @@
 												<li> Par <?php echo $comment->getName(); ?> </li>
 												<li> Le <?php echo $date['mday'] . '/' . $date['mon'] . '/' . $date['year'] . ' à ' . $date['hours'] . 'h:' . $date['minutes'] . 'min'; ?> </li>
 											</li>
-											<form action="#" method="post">
-												<button class="btSignaler" value="comment_<?php echo $comment->getId(); ?>" name="btSignaler"> Signaler </button>
-											</form>
+											<?php
+												if(!empty($_SESSION['auth']))
+												{
+													?>
+														<form action="#" method="post">
+															<button class="btSignaler" value="comment_<?php echo $comment->getId(); ?>" name="btSignaler"> Signaler </button>
+														</form>
+													<?php
+													if($_SESSION['membre']->getAccessLevel() >= 4)
+													{
+														?>
+																<a href="admin/?delComment=<?php echo $comment->getId(); ?>"> <button> Supprimer </button> </a>
+														<?php
+													}
+												}
+											?>
 										</article>
 									<?php
 								}
