db3a975a342ccaed43acbf15d09bb43c28284fec
Goufoux <deadworldcorp@gmail.com>
2018-08-16 11:20:40 +0200

    Ajout de la vérification de l'existence de la variable

--- App/Frontend/Modules/Index/Views/showBillet.php
+++ App/Frontend/Modules/Index/Views/showBillet.php
@@ -1,5 +1,7 @@
 <link rel="stylesheet" href="css/frontend/showBillet.css" />
 <?php
+	if(!empty($billet))
+	{
 		if(!empty($_SESSION['auth']) AND $_SESSION['auth'] AND $_SESSION['membre']->getAccessLevel() >= 3)
 		{
 			?>
@@ -9,71 +11,72 @@
 				</nav>
 			<?php
 		}
-	?>
-<article class="col-12 col-md-10 col-lg-8 billet">
-	<h3> <?php echo $billet->getTitre(); ?> </h3>
-	<div class="content">
-		<?php echo nl2br($billet->getContenu()); ?>
-	</div>
-	<ul>
-		<?php
-			$date = getDate($billet->getDatePub());
 		?>
-		<li> Publié le: <?php echo $date['mday'] . "/" . $date['mon'] . "/" . $date['year']; ?> </li>
-			<?php
-				if($billet->getDateMod())
-				{
-					$act = getDate();
-					$diff = getDate($act[0] - $billet->getDateMod());
-					?>
-						<li> Modifié il y a : <?php echo $diff['hours'] . ':' . $diff['minutes'] . ':' . $diff['seconds']; ?> </li>
+			<article class="col-12 col-md-10 col-lg-8 billet">
+				<h3> <?php echo $billet->getTitre(); ?> </h3>
+				<div class="content">
+					<?php echo nl2br($billet->getContenu()); ?>
+				</div>
+				<ul>
 					<?php
-				}
-			?>
-</article>
-<section class="boxComment col-12">
-	<form action="#" method="post" class="col-lg-3 col-md-8 col-sm-12 formComment">
-		<h4> Laissez un commentaire ! </h4>
-		<input type="name" id="cName" class="cName col-12" name="cName" placeholder="Nom" required />
-		<input type="email" id="cEmail" class="cEmail col-12" name="cEmail" placeholder="Email" required />
-		<input type="hidden" name="bId" value="<?php echo $billet->getId();?>" />
-		<textarea class="cDesc col-12" id="cDesc" name="cDesc" placeholder="Commentaire..." required></textarea>
-		<input type="submit" value="Commenter" />
-	</form>
-	<div class="comment col-lg-7 col-md-8 col-sm-12">
-		<h3> Liste des commentaires : </h3>
-			<?php
-				if(!empty($listComment))
-				{
-					foreach($listComment as $comment)
-					{
-						$date = getDate($comment->getDatePub());
-						?>
-							<article class="col-6">
-								<p>
-									<?php echo nl2br($comment->getContenu()); ?>
-								</p>
-								<ul>
-									<li> Par <?php echo $comment->getName(); ?> </li>
-									<li> Le <?php echo $date['mday'] . '/' . $date['mon'] . '/' . $date['year'] . ' à ' . $date['hours'] . 'h:' . $date['minutes'] . 'min'; ?> </li>
-								</li>
-								<form action="#" method="post">
-									<button class="btSignaler" value="comment_<?php echo $comment->getId(); ?>" name="btSignaler"> Signaler </button>
-								</form>
-							</article>
+						$date = getDate($billet->getDatePub());
+					?>
+					<li> Publié le: <?php echo $date['mday'] . "/" . $date['mon'] . "/" . $date['year']; ?> </li>
 						<?php
-					}
-				}
-				else
-				{
-					?>
-						<p> Aucun commentaire pour le moment. </p>
-					<?php
-				}
-			?>
-		</div>
-</section>
-<?php
+							if($billet->getDateMod())
+							{
+								$act = getDate();
+								$diff = getDate($act[0] - $billet->getDateMod());
+								?>
+									<li> Modifié il y a : <?php echo $diff['hours'] . ':' . $diff['minutes'] . ':' . $diff['seconds']; ?> </li>
+								<?php
+							}
+						?>
+			</article>
+			<section class="boxComment col-12">
+				<form action="#" method="post" class="col-lg-3 col-md-8 col-sm-12 formComment">
+					<h4> Laissez un commentaire ! </h4>
+					<input type="name" id="cName" class="cName col-12" name="cName" placeholder="Nom" required />
+					<input type="email" id="cEmail" class="cEmail col-12" name="cEmail" placeholder="Email" required />
+					<input type="hidden" name="bId" value="<?php echo $billet->getId();?>" />
+					<textarea class="cDesc col-12" id="cDesc" name="cDesc" placeholder="Commentaire..." required></textarea>
+					<input type="submit" value="Commenter" />
+				</form>
+				<div class="comment col-lg-7 col-md-8 col-sm-12">
+					<h3> Liste des commentaires : </h3>
+						<?php
+							if(!empty($listComment))
+							{
+								foreach($listComment as $comment)
+								{
+									$date = getDate($comment->getDatePub());
+									?>
+										<article class="col-6">
+											<p>
+												<?php echo nl2br($comment->getContenu()); ?>
+											</p>
+											<ul>
+												<li> Par <?php echo $comment->getName(); ?> </li>
+												<li> Le <?php echo $date['mday'] . '/' . $date['mon'] . '/' . $date['year'] . ' à ' . $date['hours'] . 'h:' . $date['minutes'] . 'min'; ?> </li>
+											</li>
+											<form action="#" method="post">
+												<button class="btSignaler" value="comment_<?php echo $comment->getId(); ?>" name="btSignaler"> Signaler </button>
+											</form>
+										</article>
+									<?php
+								}
+							}
+							else
+							{
+								?>
+									<p> Aucun commentaire pour le moment. </p>
+								<?php
+							}
+						?>
+					</div>
+			</section>
+		<?php
+	}
 	if(!empty($error))
 	{
 		?>
