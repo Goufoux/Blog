1c56fd3e667c6c3b14fd81ea0ab1236e09820303
Goufoux <deadworldcorp@gmail.com>
2018-08-16 15:06:10 +0200

    Mise à jour du formulaire

--- App/Frontend/Modules/Index/Views/connect.php
+++ App/Frontend/Modules/Index/Views/connect.php
@@ -1,9 +1,9 @@
 <link rel="stylesheet" href="css/frontend/connect.css" />
-<h3> Connexion </h3>
+<h3> Connexion / Inscription </h3>
 <form action="#" method="post" class="formConnect col-12">
-	<input type="text" id="uLogin" class="uLogin" placeholder="Pseudo ou Email" name="uLogin" required />
+	<input type="text" id="uLogin" class="uLogin" placeholder="Pseudo" name="uLogin" required />
 	<input type="password" id="uPass" class="uPass" placeholder="Pass" name="uPass" required />
-	<input type="submit" value="Connexion" />
+	<input type="submit" value="Connexion / Inscription" />
 </form>
 <?php
 	if(!empty($error))
